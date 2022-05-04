0418d83f6bf6ea0a2ca772e8fa8c5e9fe74855ae
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-05-04 11:11:47 +0800

    0504 rmd x8 update

--- rmdx8.py
+++ rmdx8.py
@@ -1,82 +1,31 @@
 import can
 
-class Rmdx8:
+
+class RmdX8:
     def __init__(self):
-
         self.RMD_X8_SPEED_LIMITED = 514
-
-        #self.SET_SPEED_CMD = "SP"
-        self.CLOCKWISE_CMD = "TL"
-        self.COUNTER_CLOCKWISE_CMD = "TR"
-        self.READ_MULTI_TURN_ANGLE_CMD = "MT"
-
-        self.READ_SPEED_CMD = "RP"
-        self.READ_ENCODER_CMD = "RE"
-        self.READ_TEMP_CMD = "RT"
-
-        self.READ_ANGLE_KP_CMD = "AP"
-        self.READ_ANGLE_KI_CMD = "AI"
-        self.READ_SPEED_KP_CMD = "VP"
-        self.READ_SPEED_KI_CMD = "VI"
-        self.READ_TORQUE_KP_CMD = "TP"
-        self.READ_TORQUE_KI_CMD = "TI"
-
+        self.SET_TORQUE_CMD = "ST"
         self.m_rmd_x8_position = 0
-        #self.m_uart_data_receive = ""
-        self.m_uart_cmd = ""
-        self.m_uart_data = 0
-        #self.m_uart_string_complete = False
-        #self.m_float_data_value = 0
-        self.m_motor_speed = 10
-
-        self.m_get_angle_kp = False
-        self.m_get_angle_ki = False
-        self.m_get_speed_kp = False
-        self.m_get_speed_ki = False
-        self.m_get_torque_kp = False
-        self.m_get_torque_ki = False
-
-        self.m_get_speed = False
-        self.m_get_encoder = False
-        self.m_get_temp = False
-
-
+        self.m_motor_torque = 0
 
     def rmd_read(self, cmd, data):
-        #self.m_uart_cmd = cmd
-        #self.m_uart_data = data
-
-        if cmd == self.CLOCKWISE_CMD:
-            print("Set motor run clockwise")
-        elif cmd == self.COUNTER_CLOCKWISE_CMD:
-            print("Set motor run counter clockwise")
-        elif cmd == self.COUNTER_CLOCKWISE_CMD:
-            print("Set motor run counter clockwise")
-        elif cmd == self.COUNTER_CLOCKWISE_CMD:
-            print("Set motor run counter clockwise")
-        elif cmd == self.COUNTER_CLOCKWISE_CMD:
-            print("Set motor run counter clockwise")
-        elif cmd == self.COUNTER_CLOCKWISE_CMD:
-            print("Set motor run counter clockwise")
-        elif cmd == self.COUNTER_CLOCKWISE_CMD:
-            print("Set motor run counter clockwise")
-        elif cmd == self.COUNTER_CLOCKWISE_CMD:
-            print("Set motor run counter clockwise")
-        elif cmd == self.COUNTER_CLOCKWISE_CMD:
-            print("Set motor run counter clockwise")
+        if cmd == self.SET_TORQUE_CMD:
+            print("Set torque for motor run")
+            self.m_motor_torque = data  # data = torque(Nm)
+            data_out = [0xA1, 0x00, 0x00, 0x00, hex(data & 0xFF), hex(data >> 8), 0x00, 0x00]
+            send_one(data_out)
 
 
+def send_one(data=None):
+    if data is None:
+        data = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+    with can.interface.Bus() as bus:
+        msg = can.Message(
+            arbitration_id=0x141, data=data, is_extended_id=False
+        )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        try:
+            bus.send(msg)
+            print(f"Message sent on {bus.channel_info}")
+        except can.CanError:
+            print("Message NOT sent")
---
+++ test.py
@@ -0,0 +1,3 @@
+data = 1023
+
+print(hex(data&0xFF), hex(data>>8))
